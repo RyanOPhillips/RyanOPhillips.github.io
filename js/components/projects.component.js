ae8539af3c7cbdcca17268b284ab6b69cd76a4c8
Ryan Phillips <rphillips4417@gmail.com>
2017-05-16 16:48:45 -0600

    Navbar working and projects code started

--- js/components/projects.component.js
+++ js/components/projects.component.js
@@ -7,7 +7,8 @@
       controller: ProjectsController
     })
   function ProjectsController(){
-
+    const vm = this
+    vm.projects =[{title: "Title 1", description: "First project description", imageURL: "https://www.planwallpaper.com/static/images/cool_cat.jpg"}, {title: "Title 2", description: "Second project description", imageURL: "https://www.planwallpaper.com/static/images/cool_cat.jpg"}, {title: "Title 3", description: "Third project description", imageURL: "https://www.planwallpaper.com/static/images/cool_cat.jpg"}]
 
   }
 }())
