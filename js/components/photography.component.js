bd92cdf6348c8216b4fa8967e96d38196f225cee
Ryan Phillips <rphillips4417@gmail.com>
2017-05-25 12:07:03 -0600

    added photos and icon

--- js/components/photography.component.js
+++ js/components/photography.component.js
@@ -10,19 +10,21 @@
     const vm = this
 
     vm.photography = [
-      {title: "Sevilla, Spain", imageURL: "media/spain/DSCN0798.jpg"},
+      {title: "Shanghai, China", imageURL: "media/china/DSC_0855.jpg"},
       {title: "Portsalon, Ireland", imageURL: "media/ireland/DSC_2454.jpg"},
       {title: "Catedral de Sevilla, Sevilla, Spain", imageURL: "media/spain/DSCN0970.jpg"},
       {title: "Great Wall, Beijing, China", imageURL: "media/china/DSC_0228.jpg"},
-      {title: "Belfast, Northern Ireland", imageURL: "media/ireland/DSC_2680.jpg"},
+      {title: "Sevilla, Spain", imageURL: "media/spain/DSCN0798.jpg"},
       {title: "Sevilla, Spain", imageURL: "media/spain/DSCN0911.jpg"},
       {title: "Twin Lakes, Colorado", imageURL: "media/aspen/DSC_3126.jpg"},
       {title: "Great Wall, Beijing, China", imageURL: "media/china/DSC_0266.jpg"},
       {title: "Maroon Bells, Aspen, Colorado", imageURL: "media/aspen/DSC_3291.jpg"},
-      {title: "Shanghai, China", imageURL: "media/china/DSC_0855.jpg"},
+      {title: "Belfast, Northern Ireland", imageURL: "media/ireland/DSC_2680.jpg"},
       {title: "Glacier National Park, Montana", imageURL: "media/montana/DSCN3238.jpg"},
       {title: "Granada, Spain", imageURL: "media/spain/DSCN1198.jpg"},
-      {title: "Hong Kong, China", imageURL: "media/china/DSC_1363.jpg"}
+      {title: "Hong Kong", imageURL: "media/china/DSC_1363.jpg"},
+      {title: "La Alhambra, Granada, Spain", imageURL: "media/spain/DSCN1228.jpg"},
+      {title: "Catedral de Sevilla, Sevilla, Spain", imageURL: "media/spain/DSCN1406.jpg"},
     ]
 
 
